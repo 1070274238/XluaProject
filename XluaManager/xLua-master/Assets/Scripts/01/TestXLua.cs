89bc6442c06b2eb067a69a34a411125b4e08f7cb
1070274238 <46987321+1070274238@users.noreply.github.com>
2019-11-18 19:18:45 +0800

    Add:增加一部分工程文件夹和一个Lua测试项目

---
+++ XluaManager/xLua-master/Assets/Scripts/01/TestXLua.cs
@@ -0,0 +1,22 @@
+﻿//xiaowangzi;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class TestXLua : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuaEnv _lua = new LuaEnv();
+        _lua.DoString("CS.UnityEngine.Debug.Log('Hello World')");
+        _lua.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
