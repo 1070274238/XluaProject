1b27d875e92442e7b8cc64179ac1ffb0859fb741
1070274238 <46987321+1070274238@users.noreply.github.com>
2019-11-18 19:35:06 +0800

    Modify:修改Xlua的一些配置

--- XluaManager/xLua-master/Assets/Scripts/01/TestXLua.cs
+++ XluaManager/xLua-master/Assets/Scripts/01/TestXLua.cs
@@ -6,12 +6,29 @@ using XLua;
 
 public class TestXLua : MonoBehaviour
 {
+    [System.Serializable]
+    public class InitGameobj
+    {
+        [Header("对象名称")]
+        public string _name;
+        [Header("对象")]
+        public GameObject _obj;
+    }
+    [Header("操作的对象")]
+    public InitGameobj[] _initGameobj;
+
+    [Header("Lua的文本文件")]
+    public TextAsset _luaText;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         LuaEnv _lua = new LuaEnv();
         _lua.DoString("CS.UnityEngine.Debug.Log('Hello World')");
         _lua.Dispose();
+
     }
 
     // Update is called once per frame
